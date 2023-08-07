3035994a9133479ceca30d82006fa9515ff0bbfb
zulfuqarov <zulfuqarov.nebi@gmail.com>
2023-08-07 05:36:02 -0700

    Countr 100/100

--- src/componets/homes/sections/SectionCountr.jsx
+++ src/componets/homes/sections/SectionCountr.jsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SectionCount.css";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 const SectionCountr = () => {
+  const [scrollUp, setscrollUp] = useState(false);
+  const tureScrollUp = () => {
+    setscrollUp(true);
+  };
+  const falseScrollUp = () => {
+    setscrollUp(false);
+  };
   return (
-    <section className="Section-countr countr-any">
-      <div className="d-flex justify-content-around">
-        <i class="fa-solid fa-graduation-cap"></i>
-        <i class="fa-solid fa-id-card"></i>
-        <i class="fa-solid fa-calendar-days"></i>
-        <i class="fa-solid fa-laptop"></i>
-      </div>
-      <div className="d-flex justify-content-around countr-any">
-        <h1>1</h1>
-        <h1>2</h1>
-        <h1>3</h1>
-        <h1>4</h1>
-      </div>
-      <div className="d-flex justify-content-around countr-span">
-        <span>Success Stories</span>
-        <span>Trusted Tutors</span>
-        <span>Scheduled Events</span>
-        <span>Available Courses</span>
+    <section className="Section-countr d-flex align-items-center">
+      <div className="container">
+        <ScrollTrigger
+          className="row"
+          onEnter={tureScrollUp}
+          onExit={falseScrollUp}
+        >
+          <div className="col-12 col-md-6 col-lg-3 text-center countr-span countr-any">
+            <i class="fa-solid fa-graduation-cap"></i>
+            <h1>
+              {scrollUp && (
+                <CountUp start={0} end={13286} duration={2} delay={0} />
+              )}
+            </h1>
+            <span>Success Stories</span>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-3 text-center countr-span countr-any">
+            <i class="fa-solid fa-id-card"></i>
+            <h1>
+              {scrollUp && (
+                <CountUp start={0} end={678} duration={2} delay={0} />
+              )}
+            </h1>
+            <span>Trusted Tutors</span>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-3 text-center countr-span countr-any">
+            <i class="fa-solid fa-calendar-days"></i>
+            <h1>
+              {scrollUp && (
+                <CountUp start={0} end={347} duration={2} delay={0} />
+              )}
+            </h1>
+            <span>Scheduled Events</span>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-3 text-center countr-span countr-any">
+            <i class="fa-solid fa-laptop"></i>
+            <h1>
+              {scrollUp && (
+                <CountUp start={0} end={1912} duration={2} delay={0} />
+              )}
+            </h1>
+            <span>Available Courses</span>
+          </div>
+        </ScrollTrigger>
       </div>
     </section>
   );
